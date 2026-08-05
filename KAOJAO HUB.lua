96af2c93fa17898d6c7473d9adea4980278b39a0
sun2557xyz-jpg <sun2557xyz@gmail.com>
2026-08-05 16:20:05 +0700

    Add KAOJAO HUB V19.0 - No UI version
    Initial version of KAOJAO HUB with no UI.

---
+++ KAOJAO HUB.lua
@@ -0,0 +1,11 @@
+-- [[ KAOJAO HUB V19.0 - NO UI VERSION ]] --
+-- เครดิต: ข้าวเจ้า (Kao Jao) | ฐานจาก V6.3 ต้นฉบับ + VASCAL
+
+repeat task.wait() until game:IsLoaded()
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+local Camera = workspace.CurrentCamera
+local lp = Players.LocalPlayer
+local Mouse = lp:GetMouse
